7bdb11340e259c8f9e578cfe5b69e218d920a4cd
Dmitriy Pyshinskiy <dimapyshynskiy@gmail.com>
2023-01-15 16:22:15 +0300

    add profile repo interface

--- repository/profile.go
+++ repository/profile.go
@@ -1,1 +1,9 @@
 package repository
+
+import "github.com/PyshSoft/farinelly/entity"
+
+type ProfileRepository interface {
+	Create(entity.Profile) error
+	GetById(string) (entity.Profile, bool)
+	GetByGenger() []entity.Profile
+}
--- service/profile.go
+++ service/profile.go
@@ -1,1 +1,18 @@
 package service
+
+import (
+	"github.com/PyshSoft/farinelly/entity"
+	"github.com/PyshSoft/farinelly/repository"
+)
+
+type ProfileService struct {
+	repo repository.ProfileRepository
+}
+
+func NewProfileService(repo repository.ProfileRepository) *ProfileService {
+	return &ProfileService{repo}
+}
+
+func (ps *ProfileService) Create(profile entity.Profile) error {
+	return ps.repo.Create(profile)
+}
